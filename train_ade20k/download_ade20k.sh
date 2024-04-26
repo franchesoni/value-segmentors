922b3529eb00af0038d451b92ba56b3bfcb03e2e
franchesoni <marchesoniacland@gmail.com>
2024-04-26 14:47:41 +0200

    Add script to download ADE20K dataset

---
+++ train_ade20k/download_ade20k.sh
@@ -0,0 +1,5 @@
+mkdir -p data
+wget -O ./data/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+unzip ./data/ADEChallengeData2016.zip -d ./data
+rm ./data/ADEChallengeData2016.zip
+echo "Dataset downloaded."
